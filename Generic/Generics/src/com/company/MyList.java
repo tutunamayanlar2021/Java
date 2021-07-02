d1261dfa71aa6a6e82bfc26f868bd1a2bf6fe25e
Kader Oral <63172521+tutunamayanlar2021@users.noreply.github.com>
2021-07-02 21:50:17 +0300

    Generic ve Repository Desenini Anlamak
    Generic ve Repository Desenini Anlamak

---
+++ Generic/Generics/src/com/company/MyList.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class MyList<T> {
+    public  void add(T object){
+
+    }
+    public  void remove(T object){
+
+    }
+}
---
+++ Generic/RepositoryDemo/src/com/company/Validator.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Validator {
+    public <T> void validate(T Entity){
+
+    }
+}
